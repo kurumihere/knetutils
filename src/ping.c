8abe44036b751143e3b5d0bb7bdb7f2dc1841fa1
kurumi <kurumihere@disroot.org>
2026-06-24 12:43:43 +0500

    Format code to match .clang-format

--- src/ping.c
+++ src/ping.c
@@ -72,11 +72,14 @@ ping_run(const ping_config_t *config)
 
         if (!config->quiet) {
                 if (config->cisco_style) {
-                        printf("Sending %u, %u-byte ICMP Echos to %s, timeout is %u seconds:\n",
-                               config->count, config->payload_size, target_str,
-                               (unsigned int)(config->timeout_ns / 1000000000ULL));
+                        printf(
+                            "Sending %u, %u-byte ICMP Echos to %s, timeout is "
+                            "%u seconds:\n",
+                            config->count, config->payload_size, target_str,
+                            (unsigned int)(config->timeout_ns / 1000000000ULL));
                 } else {
-                        printf("PING %s: %u data bytes\n", target_str, config->payload_size);
+                        printf("PING %s: %u data bytes\n", target_str,
+                               config->payload_size);
                 }
         }
 
@@ -90,7 +93,6 @@ ping_run(const ping_config_t *config)
         struct pollfd pfd;
         pfd.fd = net_get_fd(sock);
         pfd.events = POLLIN;
-
 
         int icmp_type_req =
             (config->family == AF_INET6) ? ICMP6_ECHO_REQUEST : ICMP_ECHO;
@@ -149,7 +151,8 @@ ping_run(const ping_config_t *config)
 
                 uint64_t now = get_time_ns();
                 uint64_t next_send = send_time + config->interval_ns;
-                uint64_t wait_until = config->flood ? next_send : (now + config->timeout_ns);
+                uint64_t wait_until =
+                    config->flood ? next_send : (now + config->timeout_ns);
 
                 bool got_reply = false;
                 bool replied = false;
@@ -204,34 +207,45 @@ ping_run(const ping_config_t *config)
                                         r_seq = r_icp->icmp6_seq;
                                 }
 
-                                if (r_type == icmp_type_rep && (is_dgram || r_id == htons(pid))) {
+                                if (r_type == icmp_type_rep &&
+                                    (is_dgram || r_id == htons(pid))) {
                                         if (config->flood) {
-                                                uint64_t recv_time = get_time_ns();
-                                                uint64_t rtt = time_diff_ns(send_time, recv_time);
-                                                
-                                                if (rtt_min == 0 || rtt < rtt_min) rtt_min = rtt;
-                                                if (rtt > rtt_max) rtt_max = rtt;
+                                                uint64_t recv_time =
+                                                    get_time_ns();
+                                                uint64_t rtt = time_diff_ns(
+                                                    send_time, recv_time);
+
+                                                if (rtt_min == 0 ||
+                                                    rtt < rtt_min)
+                                                        rtt_min = rtt;
+                                                if (rtt > rtt_max)
+                                                        rtt_max = rtt;
                                                 rtt_sum += rtt;
-                                                
+
                                                 received++;
                                                 got_reply = true;
                                                 replied = true;
                                                 printf("\b \b");
                                                 fflush(stdout);
-                                                
-                                                if (config->count > 0 && received >= config->count) {
+
+                                                if (config->count > 0 &&
+                                                    received >= config->count) {
                                                         keep_running = false;
                                                 }
                                                 break;
                                         } else if (r_seq == htons(seq)) {
-                                                uint64_t recv_time = get_time_ns();
+                                                uint64_t recv_time =
+                                                    get_time_ns();
                                                 uint64_t rtt = 0;
 
                                                 if (recv_time >= send_time) {
-                                                        rtt = time_diff_ns(send_time, recv_time);
+                                                        rtt = time_diff_ns(
+                                                            send_time,
+                                                            recv_time);
                                                 }
 
-                                                if (rtt_min == 0 || rtt < rtt_min)
+                                                if (rtt_min == 0 ||
+                                                    rtt < rtt_min)
                                                         rtt_min = rtt;
                                                 if (rtt > rtt_max)
                                                         rtt_max = rtt;
@@ -239,27 +253,92 @@ ping_run(const ping_config_t *config)
 
                                                 char src_str[INET6_ADDRSTRLEN];
                                                 getnameinfo(
-                                                    (struct sockaddr *)&src_addr,
+                                                    (struct sockaddr
+                                                         *)&src_addr,
                                                     src_addr_len, src_str,
                                                     sizeof(src_str), NULL, 0,
                                                     NI_NUMERICHOST);
 
                                                 if (!config->quiet) {
-                                                        if (config->cisco_style) {
-                                                            printf("!");
-                                                            fflush(stdout);
+                                                        if (config
+                                                                ->cisco_style) {
+                                                                printf("!");
+                                                                fflush(stdout);
                                                         } else {
-                                                            char time_buf[64] = "N/A";
-                                                            if (config->payload_size >= 8) {
-                                                                format_time(rtt, config->time_unit, time_buf, sizeof(time_buf));
-                                                            }
-                                                            if (ttl >= 0) {
-                                                                printf("%zd bytes from %s: icmp_seq=%u ttl=%d time=%s\n",
-                                                                       n - hlen, src_str, ntohs(r_seq), ttl, time_buf);
-                                                            } else {
-                                                                printf("%zd bytes from %s: icmp_seq=%u time=%s\n",
-                                                                       n - hlen, src_str, ntohs(r_seq), time_buf);
-                                                            }
+                                                                char time_buf
+                                                                    [64] =
+                                                                        "N/A";
+                                                                if (config
+                                                                        ->payload_size >=
+                                                                    8) {
+                                                                        format_time(
+                                                                            rtt,
+                                                                            config
+                                                                                ->time_unit,
+                                                                            time_buf,
+                                                                            sizeof(
+                                                                                time_buf));
+                                                                }
+                                                                if (ttl >= 0) {
+                                                                        printf(
+                                                                            "%z"
+                                                                            "d "
+                                                                            "by"
+                                                                            "te"
+                                                                            "s "
+                                                                            "fr"
+                                                                            "om"
+                                                                            " %"
+                                                                            "s:"
+                                                                            " i"
+                                                                            "cm"
+                                                                            "p_"
+                                                                            "se"
+                                                                            "q="
+                                                                            "%u"
+                                                                            " t"
+                                                                            "tl"
+                                                                            "=%"
+                                                                            "d "
+                                                                            "ti"
+                                                                            "me"
+                                                                            "=%"
+                                                                            "s"
+                                                                            "\n",
+                                                                            n - hlen,
+                                                                            src_str,
+                                                                            ntohs(
+                                                                                r_seq),
+                                                                            ttl,
+                                                                            time_buf);
+                                                                } else {
+                                                                        printf(
+                                                                            "%z"
+                                                                            "d "
+                                                                            "by"
+                                                                            "te"
+                                                                            "s "
+                                                                            "fr"
+                                                                            "om"
+                                                                            " %"
+                                                                            "s:"
+                                                                            " i"
+                                                                            "cm"
+                                                                            "p_"
+                                                                            "se"
+                                                                            "q="
+                                                                            "%u"
+                                                                            " t"
+                                                                            "im"
+                                                                            "e="
+                                                                            "%s"
+                                                                            "\n",
+                                                                            n - hlen,
+                                                                            src_str,
+                                                                            ntohs(
+                                                                                r_seq),
+                                                                            time_buf);
+                                                                }
                                                         }
                                                 }
 
@@ -289,8 +368,9 @@ ping_run(const ping_config_t *config)
                 if (keep_running &&
                     (config->count == 0 || sent < config->count)) {
                         uint64_t current = get_time_ns();
-                        uint64_t next_send_time = send_time + config->interval_ns;
-                        
+                        uint64_t next_send_time =
+                            send_time + config->interval_ns;
+
                         if (config->flood && got_reply) {
                                 /* send immediately */
                         } else if (current < next_send_time) {
@@ -304,26 +384,40 @@ ping_run(const ping_config_t *config)
         if (!config->quiet) {
                 if (config->cisco_style) {
                         printf("\nSuccess rate is %u percent (%u/%u)",
-                               sent == 0 ? 0 : ((received * 100) / sent), received, sent);
+                               sent == 0 ? 0 : ((received * 100) / sent),
+                               received, sent);
                         if (received > 0 && config->payload_size >= 8) {
                                 char min_buf[64], avg_buf[64], max_buf[64];
-                                format_time(rtt_min, config->time_unit, min_buf, sizeof(min_buf));
-                                format_time(rtt_sum / received, config->time_unit, avg_buf, sizeof(avg_buf));
-                                format_time(rtt_max, config->time_unit, max_buf, sizeof(max_buf));
-                                printf(", round-trip min/avg/max = %s/%s/%s", min_buf, avg_buf, max_buf);
+                                format_time(rtt_min, config->time_unit, min_buf,
+                                            sizeof(min_buf));
+                                format_time(rtt_sum / received,
+                                            config->time_unit, avg_buf,
+                                            sizeof(avg_buf));
+                                format_time(rtt_max, config->time_unit, max_buf,
+                                            sizeof(max_buf));
+                                printf(", round-trip min/avg/max = %s/%s/%s",
+                                       min_buf, avg_buf, max_buf);
                         }
                         printf("\n");
                 } else {
                         printf("\n--- %s ping statistics ---\n", target_str);
-                        printf("%u packets transmitted, %u packets received, %d%% packet loss\n",
-                               sent, received, sent == 0 ? 0 : ((sent - received) * 100) / sent);
+                        printf("%u packets transmitted, %u packets received, "
+                               "%d%% packet loss\n",
+                               sent, received,
+                               sent == 0 ? 0
+                                         : ((sent - received) * 100) / sent);
 
                         if (received > 0 && config->payload_size >= 8) {
                                 char min_buf[64], avg_buf[64], max_buf[64];
-                                format_time(rtt_min, config->time_unit, min_buf, sizeof(min_buf));
-                                format_time(rtt_sum / received, config->time_unit, avg_buf, sizeof(avg_buf));
-                                format_time(rtt_max, config->time_unit, max_buf, sizeof(max_buf));
-                                printf("rtt min/avg/max = %s/%s/%s\n", min_buf, avg_buf, max_buf);
+                                format_time(rtt_min, config->time_unit, min_buf,
+                                            sizeof(min_buf));
+                                format_time(rtt_sum / received,
+                                            config->time_unit, avg_buf,
+                                            sizeof(avg_buf));
+                                format_time(rtt_max, config->time_unit, max_buf,
+                                            sizeof(max_buf));
+                                printf("rtt min/avg/max = %s/%s/%s\n", min_buf,
+                                       avg_buf, max_buf);
                         }
                 }
         }
